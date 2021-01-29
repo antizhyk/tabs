ab3d24696ae284256fb3ed0b6a3446bfc034f4b9
antizyk <65877663+antizyk@users.noreply.github.com>
2021-01-29 20:52:45 +0100

    Табы

---
+++ js/script.js
@@ -0,0 +1,39 @@
+'use strict';
+function tabs(tabsSelector, tabsContentSelector, tabsParentSelector, activeClass) {
+	//Tabs
+	const tabs = document.querySelectorAll(tabsSelector),
+		tabsContent = document.querySelectorAll(tabsContentSelector),
+		parentTabs = document.querySelector(tabsParentSelector);
+
+	function hideTabs() {
+		tabsContent.forEach(item => {
+			item.classList.add('hide');
+			item.classList.remove('show');
+		});
+		tabs.forEach(item => {
+			item.classList.remove(activeClass);
+		});
+	}
+	function showTabs(i = 0) {
+		tabsContent[i].classList.add('show');
+		tabsContent[i].classList.remove('hide');
+		tabs[i].classList.add(activeClass);
+	}
+
+	hideTabs();
+	showTabs();
+
+	parentTabs.addEventListener('click', e => {
+		let targetElement = e.target;
+
+		if (targetElement && targetElement.classList.contains(tabsSelector.slice(1))) {
+			tabs.forEach((item, i) => {
+				if (tabs[i] == targetElement) {
+					hideTabs();
+					showTabs(i);
+				}
+			});
+		}
+	});
+}
+tabs('.tabheader__item', '.tabcontent', '.tabheader__items', 'tabheader__item_active');
